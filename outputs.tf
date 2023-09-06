3a07e977af3da59e41c4843f0207c82aa44c709e
Dave Costakos <david.costakos@redhat.com>
2023-09-06 12:59:09 -0700

    Update outputs.tf
    fixing output map

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "ec2_map" {
-  value = { for i in aws_instance.ec2instance : i.tags.Name => "${i.id}:${i.public_ip}" }
+  value = { for i in aws_instance.instances : i.tags.Name => "${i.id}:${i.public_ip}" }
 }
